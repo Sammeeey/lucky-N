f0f570e040f6062363332bebd9ddf6bb81dd8cd1
Samuel Hartmann <hartmann.samuel@gmx.de>
2023-11-05 15:29:15 +0100

    calculate + display result sum & winner

--- src/LuckyN.jsx
+++ src/LuckyN.jsx
@@ -1,19 +1,31 @@
 // import './LuckyN.css'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Dice from "./Dice"
 
 function LuckyN({dieAmount=2, dieMaxValue=6, winningNum=10}) {
     const dieArr = [...Array(dieAmount).keys()]
     const [dice, setDice] = useState(dieArr.map(die => ({rollResult:null, dieMaxValue:dieMaxValue})))
+    const [resultSum, setResultSum] = useState(null)
 
     const rollDie = () => 
         setDice(prevDice => prevDice.map(die => (
             {...die, rollResult: Math.floor(Math.random() * dieMaxValue + 1)}
         )))
 
+    const getResultSum = () => {
+        const rollSum = dice.reduce((sum, currentValue) => sum + currentValue.rollResult, 0)
+        setResultSum(prevResultSum => prevResultSum = rollSum)
+        console.log('rollSum', rollSum)
+    }
+
+    useEffect(getResultSum, [dice, winningNum])
+
     return (
         <>
+        <div>winningNum: {winningNum}</div>
+        <div>resultSum: {resultSum}</div>
+        <div>{resultSum === winningNum && 'Game Won!'}</div>
         <Dice {...{dice}} />
         <button type="button" onClick={rollDie}>roll die</button>
         </>
